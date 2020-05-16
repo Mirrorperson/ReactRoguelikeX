eb5f0bae7c475b2dc0bb677645bf810660465630
Mirrorperson <rabindra555@hotmail.com>
2020-05-16 02:13:47 +0100

    Position and tileCodeAgentOn kept in Agent class
     #25

--- src/components/Agent.js
+++ src/components/Agent.js
@@ -19,9 +19,21 @@ class Agent extends Component {
       dex: AgentsData[props.type].dex,
       wis: AgentsData[props.type].wis,
       int: AgentsData[props.type].int,
-      position: position
+      position: position,
+      tileCodeAgentOn: 1 // could replace with random tile
     };
   }
+
+  // Need to make this generic update agent position
+  UpdateAgent = (
+    newPlayerPosition,
+    mapState
+    // Flip when use as rows in columns means need [y,x]
+  ) => {
+    this.state.tileCodeAgentOn =
+      mapState[newPlayerPosition[1]][newPlayerPosition[0]];
+    this.state.position = newPlayerPosition;
+  };
 }
 
 export default Agent;
