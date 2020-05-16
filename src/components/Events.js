1255e4034dadb7f965fee5bf405beb06f36e08ac
Mirrorperson <rabindra555@hotmail.com>
2020-05-16 02:28:37 +0100

    Refactor movement into Events, add agent move func
     #25

---
+++ src/components/Events.js
@@ -0,0 +1,153 @@
+import { GetAgentWithId, ConsoleLogTest, RollRandom } from '../Utility';
+import _ from 'lodash';
+
+const GetNewAgentPosition = (
+  oldPosition,
+  eventKey,
+  columnsNum,
+  rowsNum,
+  frontGap,
+  backGap
+) => {
+  let newPosition;
+
+  // TileStates is arrays within array; first index is rows, second index is columns.
+  // Swap indexes on setting position - 0 index horizontal, 1 index vertical
+  switch (eventKey) {
+    case 'ArrowLeft':
+      newPosition = [oldPosition[0] - 1, oldPosition[1]];
+      break;
+    case 'ArrowRight':
+      newPosition = [oldPosition[0] + 1, oldPosition[1]];
+      break;
+    case 'ArrowUp':
+      newPosition = [oldPosition[0], oldPosition[1] - 1];
+      break;
+    case 'ArrowDown':
+      newPosition = [oldPosition[0], oldPosition[1] + 1];
+      break;
+    default:
+      newPosition = oldPosition;
+  }
+
+  // check boundaries, affect player agent only
+  if (
+    newPosition[0] < frontGap ||
+    newPosition[0] >= columnsNum - backGap ||
+    newPosition[1] < frontGap ||
+    newPosition[1] >= rowsNum - backGap
+  ) {
+    return;
+  }
+
+  return newPosition;
+};
+
+// Need to make this generic update agent position
+const UpdateAgentPosition = (
+  agent,
+  newAgentPosition,
+  oldAgentPosition,
+  tilesStates
+  // Flip when use as colums in rows means need [y,x]
+) => {
+  let newState = tilesStates.slice(0);
+  // Add old tile code player is on
+  newState[oldAgentPosition[1]][oldAgentPosition[0]] =
+    agent.state.tileCodeAgentOn;
+
+  agent.UpdateAgent(newAgentPosition, tilesStates);
+
+  // Add agent to new tile
+  newState[newAgentPosition[1]][newAgentPosition[0]] = agent.state.id;
+
+  return newState;
+};
+
+const HandleEvent = (agentId, oldAgentPosition, eventKey, state) => {
+  // check keys
+  ConsoleLogTest(state.test, 'key pressed ' + eventKey);
+
+  let tilesStates = state.tilesStates;
+  let newAgents = state.agents.map((a) => Object.assign({}, a));
+  let agent = GetAgentWithId(agentId, newAgents);
+
+  let newAgentPosition = GetNewAgentPosition(
+    oldAgentPosition,
+    eventKey,
+    state.columns,
+    state.rows,
+    state.playerPosFrontEdgeGap,
+    state.playerPosBackEdgeGap
+  );
+
+  // no movement, collison or invalid
+  if (typeof newAgentPosition === 'undefined') return;
+
+  let newTilesStates = UpdateAgentPosition(
+    agent,
+    newAgentPosition,
+    oldAgentPosition,
+    tilesStates
+  );
+
+  return {
+    agents: newAgents,
+    tilesStates: newTilesStates // then move player
+  };
+};
+
+const HandleAgentEvents = (agents, state) => {
+  let activeAgent;
+  let rollUpperLimits = [];
+  let lastUpperLimit;
+
+  agents.forEach((a, i) => {
+    lastUpperLimit = i === 0 ? 0 : rollUpperLimits[i - 1];
+    rollUpperLimits.push(a.state.agi + RollRandom(10) + lastUpperLimit);
+  });
+
+  // Roll up to max of sum
+  let activeAgentRoll = RollRandom(rollUpperLimits[rollUpperLimits.length - 1]);
+
+  rollUpperLimits.forEach((l, i) => {
+    if (activeAgent === undefined && activeAgentRoll < l) {
+      activeAgent = agents[i];
+    }
+  });
+
+  if (activeAgent.state.type === 'player') {
+    return;
+  }
+
+  // Todo make decision making smarter
+  // may need to move to agent class if quite large
+  let directionRoll = RollRandom(4);
+  let eventKey;
+
+  switch (directionRoll) {
+    case 1:
+      eventKey = 'ArrowLeft';
+      break;
+    case 2:
+      eventKey = 'ArrowUp';
+      break;
+    case 3:
+      eventKey = 'ArrowRight';
+      break;
+    case 4:
+      eventKey = 'ArrowDown';
+      break;
+  }
+
+  let newState = HandleEvent(
+    activeAgent.state.id,
+    activeAgent.state.position,
+    eventKey,
+    state
+  );
+
+  return newState;
+};
+
+export { HandleEvent, UpdateAgentPosition, HandleAgentEvents };
--- src/components/Map.js
+++ src/components/Map.js
@@ -55,69 +55,29 @@ class Map extends Component {
     };
   }
 
-  updatePlayerPos = (
-    newPlayerPosition,
-    oldPlayerPosition
-    // Flip when use as colums in rows means need [y,x]
-  ) => {
-    let newState = this.state.tilesStates.slice(0);
-    // Add old tile player is on
-    newState[oldPlayerPosition[1]][oldPlayerPosition[0]] = this.state.playerOn;
-
-    // Add player to new tile - a0 is player agent's id
-    newState[newPlayerPosition[1]][newPlayerPosition[0]] = 'a0';
-
-    return newState;
-  };
-
   // movement controls
-  handleKeyPress = event => {
-    // check keys
-    ConsoleLogTest(this.state.test, 'key pressed ' + event.key);
+  handleKeyPress = (event) => {
+    let player = GetAgentWithId(GetPlayerAgentId(), this.state.agents);
+    let oldPlayerPosition = [...player.state.position];
 
-    let newAgents = [...this.state.agents];
-    let oldPlayerPosition = [...GetAgentWithId('a0', newAgents).state.position];
-    let newPlayerPosition;
+    let newState = HandleEvent(
+      GetPlayerAgentId(),
+      oldPlayerPosition,
+      event.key,
+      this.state
+    );
 
-    // TileStates is an array in an array; first array index is rows and send array index is columns.
-    // Fist index controls vertical movement, second index controls horizontal movement
-    switch (event.key) {
-      case 'ArrowLeft':
-        newPlayerPosition = [oldPlayerPosition[0] - 1, oldPlayerPosition[1]];
-        if (newPlayerPosition[0] <= 0) return;
-        break;
-
-      case 'ArrowRight':
-        newPlayerPosition = [oldPlayerPosition[0] + 1, oldPlayerPosition[1]];
-        if (newPlayerPosition[0] >= this.props.columns - 1) return;
-        break;
-
-      case 'ArrowUp':
-        newPlayerPosition = [oldPlayerPosition[0], oldPlayerPosition[1] - 1];
-        if (newPlayerPosition[1] <= 0) return;
-        break;
-
-      case 'ArrowDown':
-        newPlayerPosition = [oldPlayerPosition[0], oldPlayerPosition[1] + 1];
-        if (newPlayerPosition[1] >= this.props.rows - 1) return;
-        break;
-
-      default:
-        newPlayerPosition = oldPlayerPosition;
-    }
-
-    GetAgentWithId('a0', newAgents).state.position = newPlayerPosition;
-
-    if (newPlayerPosition !== oldPlayerPosition) {
-      this.setState(state => ({
-        agents: newAgents,
-        playerOn: this.state.tilesStates[newPlayerPosition[1]][
-          newPlayerPosition[0]
-        ], // Order matters store tile player moving to
-        tileStates: this.updatePlayerPos(newPlayerPosition, oldPlayerPosition) // then move player
+    if (!_.isEqual(player.state.position, oldPlayerPosition)) {
+      this.setState((state) => ({
+        agents: newState.agents,
+        tileStates: newState.tileStates
       }));
     }
   };
+
+
+      }));
+    }
 
   render() {
     ConsoleLogTest(this.state.test, this.state.tilesAgentsStates);
