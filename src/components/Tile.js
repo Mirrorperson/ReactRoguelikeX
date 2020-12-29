e0c5dfb23d6b50edf3c0e71632a3ed43d09f6c3f
Mirrorperson <rabindra555@hotmail.com>
2020-12-29 18:40:25 +0000

    Fix syntax, case start from 0, add ids for animate
     #25

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const Tile = props => {
+const Tile = (props) => {
   let tile;
 
   switch (props.tileType) {
-    case 1:
+    case 0:
       tile = <div className="tile grass" />;
       break;
-    case 2:
+    case 1:
       tile = <div className="tile rock" />;
       break;
-    case 3:
+    case 2:
       tile = <div className="tile tree" />;
       break;
-    case 4:
+    case 3:
       tile = <div className="tile water" />;
       break;
     default:
-      tile = <div className={props.tileType} />;
+      tile = <div id={props.id} className={props.tileType} />;
   }
 
   return tile;
